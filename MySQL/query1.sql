38d2782c8c35c468c13882dc4c08d921f703e707
Gian Emanuel da Silva <105940828+gianz1n@users.noreply.github.com>
2024-10-07 10:17:02 -0300

    Primeiro DB

---
+++ MySQL/query1.sql
@@ -0,0 +1,15 @@
+create database cadastro;
+use cadastro;
+
+create table pessoas(
+nome varchar(30),
+idade tinyint,
+peso float,
+altura float,
+sexo char(1),
+nacionalidade varchar(20));
+
+
+#describe pessoas;
+#show database;
+#drop database cadastro;
